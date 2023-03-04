6a11abe83ffdcd3bb8bcb535626e90fc246bc695
Brian <bfeddes@hotmail.com>
2023-03-03 23:45:10 -0600

    Added specific points per category

--- index.js
+++ index.js
@@ -19,60 +19,107 @@ app.get('/calculate', (req, res) => {
 	
 	// Parsing the data from the queryString so we can use it
 	let sum = 0
+	let agePoints, bmiPoints, bpPoints, cancerPoints, diabetesPoints, alzPoints
 	let age = parseInt(data.age)
 	let height = parseInt(data.height)
 	let weight = parseInt(data.weight)
 	let sysBloodPressure = parseInt(data.sysBloodPressure)
 	let diaBloodPressure = parseInt(data.diaBloodPressure)
 	let bmi = (weight * 0.45359237) / (height * 0.0254)**2
-	// TODO: add cancer, alzherimers, diabetes
 
 	// Adding cancer. alzheimers, and diabetes to receieve data from client - JB
 	let cancer = data.cancer === 'Yes' ? true : false
 	let alzheimers = data.alzheimers === 'Yes' ? true : false
 	let diabetes = data.diabetes === 'Yes' ? true : false
 
+	// Adding points for cancer, alz, and diabetes
+	if (cancer) {
+		cancerPoints = 10
+		sum += cancerPoints
+	} else {
+		cancerPoints = 0
+		sum += cancerPoints
+	}
+
+	if (alzheimers) {
+		alzPoints = 10
+		sum += alzPoints
+	} else {
+		alzPoints = 0
+		sum += alzPoints
+	}
+
+	if (diabetes) {
+		diabetesPoints = 10
+		sum += diabetesPoints
+	} else {
+		diabetesPoints = 0
+		sum += diabetesPoints
+	}
+
 	// Calculating points added based on age
 	if (age < 30){
-		sum += 0
+		agePoints = 0
+		sum += agePoints
 	} else if (age >= 30 && age < 45) {
-		sum += 10
+		agePoints = 10
+		sum += agePoints
 	} else if (age >= 45 && age < 60) {
-		sum += 20
+		agePoints = 20
+		sum += agePoints
 	} else {
-		sum += 30
+		agePoints = 30
+		sum += agePoints
 	}
 
 	// Calculating BMI
 	if (bmi >= 18.5 && bmi <= 24.9) {
-		sum += 0
+		bmiPoints = 0
+		sum += bmiPoints
 	} else if (bmi >= 25.0 && bmi <= 29.9) {
-		sum += 30
+		bmiPoints = 30
+		sum += bmiPoints
 	} else {
-		sum += 75
+		bmiPoints = 75
+		sum += bmiPoints
 	}
 
 	// Calculating Blood Pressue
-	if (sysBloodPressure >= 180 || diaBloodPressure >= 120){
-		sum += 100
+	if (sysBloodPressure > 180 || diaBloodPressure > 120){
+		bpPoints = 100
+		sum += bpPoints
 	} else if (sysBloodPressure >= 140 || diaBloodPressure >= 90){
-		sum += 75
-	} else if (sysBloodPressure >= 130 || diaBloodPressure >= 80){
-		sum += 30
-	} else if (sysBloodPressure >= 120 && diaBloodPressure < 80){
-		sum += 15
+		bpPoints = 75
+		sum += bpPoints
+	} else if ((sysBloodPressure >= 130 && sysBloodPressure < 140) || (diaBloodPressure >= 80 && diaBloodPressure < 90)){
+		bpPoints = 30
+		sum += bpPoints
+	} else if ((sysBloodPressure >= 120 && sysBloodPressure < 130) && diaBloodPressure < 80){
+		bpPoints = 15
+		sum += bpPoints
+	} else if (sysBloodPressure < 120 && diaBloodPressure < 80) {
+		bpPoints = 0
+		sum += bpPoints
 	}
  
 	// Calculating the results
 	res.type('text/plain')
 	if (sum <= 20) {
-		res.send(`The person has ${sum} points, meaning they are at low risk.`)
+		res.send(`Points added based on age: ${agePoints}<br>Points added based on BMI: ${bmiPoints}<br>Points added based on blood pressure: ${bpPoints}<br>
+		Points added based on cancer risk: ${cancerPoints}<br>Points added based on diabetes risk: ${diabetesPoints}<br>
+		Points added based on alzheimers risk: ${alzPoints}<br>The person's total points is ${sum}, meaning they are at low risk.`)
 	} else if (sum > 20 && sum <= 50) {
-		res.send(`The person has ${sum} points, meaning they are at moderate risk.`)
+		res.send(`Points added based on age: ${agePoints}<br>Points added based on BMI: ${bmiPoints}<br>Points added based on blood pressure: ${bpPoints}<br>
+		Points added based on cancer risk: ${cancerPoints}<br>Points added based on diabetes risk: ${diabetesPoints}<br>
+		Points added based on alzheimers risk: ${alzPoints}<br>The person's total points is ${sum}, meaning they are at moderate risk.`)
 	} else if (sum > 50 && sum <= 75) {
-		res.send(`The person has ${sum} points, meaning they are at high risk.`)
+		res.send(`Points added based on age: ${agePoints}<br>Points added based on BMI: ${bmiPoints}<br>Points added based on blood pressure: ${bpPoints}<br>
+		Points added based on cancer risk: ${cancerPoints}<br>Points added based on diabetes risk: ${diabetesPoints}<br>
+		Points added based on alzheimers risk: ${alzPoints}<br>The person's total points is ${sum}, meaning they are at high risk.`)
 	} else {
-		res.send(`The person has ${sum} points, meaning they are uninsurable.`)
+		res.send(`Points added based on age: ${agePoints}<br>Points added based on BMI: ${bmiPoints}<br>Points added based on blood pressure: ${bpPoints}<br>
+		Points added based on cancer risk: ${cancerPoints}<br>Points added based on diabetes risk: ${diabetesPoints}<br>
+		Points added based on alzheimers risk: ${alzPoints}<br>The person's total points is ${sum}, meaning they are uninsurable`)
 	}
 });
 
